fa608e7b0e6c43e0631f84a7cc2316fe46a76afe
Jelani Jackson <jelani.jackson@zapier.com>
2024-06-13 18:49:03 -0400

    Switched mux out for gin

--- app/backend/main.go
+++ app/backend/main.go
@@ -1,41 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Welcome to Too Tasty of a Trade!")
 
-	r := mux.NewRouter()
+	r := gin.Default()
 
-	var JsonMiddleware mux.MiddlewareFunc = func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-
-			next.ServeHTTP(w, r)
+	r.GET("/health-check", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "You've made it",
 		})
-	}
+	})
 
-	HealthCheck := func(w http.ResponseWriter, r *http.Request) {
-		data := map[string]interface{}{
-			"message": "Welcome to the dark side",
-		}
-
-		w.WriteHeader(http.StatusOK)
-
-		jsonData, _ := json.Marshal(data)
-
-		fmt.Fprint(w, string(jsonData))
-	}
-
-	r.Use(JsonMiddleware)
-
-	r.HandleFunc("/health-check", HealthCheck).Methods("GET")
-
-	http.ListenAndServe(":3333", r)
+	r.Run(":3000")
 }
