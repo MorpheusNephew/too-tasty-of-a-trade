461eb3b32ef7f6770d2df007e775eab385f7ce30
Jelani Jackson <jelani.jackson@zapier.com>
2024-06-19 19:59:44 -0400

    Added utils to handle converting response to json and preparing request bodies

---
+++ app/backend/internal/tastyworks/utils.go
@@ -0,0 +1,30 @@
+package tastyworks
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func convertResponseToJson[T any](resp *http.Response, jsonBody *T) error {
+	bodyBytes, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(bodyBytes, &jsonBody)
+}
+
+func prepareRequestBody[T any](requestData T) (*bytes.Buffer, error) {
+	jsonBytes, err := json.Marshal(requestData)
+
+	if err != nil {
+		return nil, err
+	}
+
+	body := bytes.NewBuffer(jsonBytes)
+
+	return body, nil
+}
