8fac4a3385485ae4c2e789f100123f21eff58f7d
Jelani Jackson <jelani.jackson@zapier.com>
2024-06-19 20:07:38 -0400

    Started working on the option chain logic

--- app/backend/internal/tastyworks/optionChain.go
+++ app/backend/internal/tastyworks/optionChain.go
@@ -4,6 +4,21 @@ import "fmt"
 
 var optionChainsUrl = fmt.Sprintf("%s/option-chains", baseUrl)
 
-func (t *TTClient) GetOptionChain(symbol string) {
-	fmt.Println("Option chains", optionChainsUrl)
+func (t *TTClient) GetOptionChain(symbol string) error {
+	fmt.Println("Option chains", optionChainsUrl, symbol)
+	url := fmt.Sprintf("%s/%s", optionChainsUrl, symbol)
+
+	resp, err := t.get(url)
+
+	if err != nil {
+		return err
+	}
+
+	responseBody := make(map[string]any)
+
+	err = convertResponseToJson(resp, &responseBody)
+
+	fmt.Println("What is happening with this option chain?", responseBody)
+
+	return err
 }
