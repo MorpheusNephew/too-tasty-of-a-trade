1bc1c3ebe919b8f1c85119c1cdf6d7891258a05b
Jelani Jackson <jelani.jackson@zapier.com>
2024-06-27 19:45:56 -0400

    Only returning what's needed

--- app/backend/internal/tastyworks/marketData.go
+++ app/backend/internal/tastyworks/marketData.go
@@ -5,35 +5,17 @@ import (
 	"strings"
 )
 
-type OptionExpirationImpliedVolatility struct {
-	ExpirationDate    string `json:"expiration-date"`
-	SettlementType    string `json:"settlement-type"`
-	OptionChainType   string `json:"option-chain-type"`
-	ImpliedVolatility string `json:"implied-volatility"`
-}
-
 type MarketMetricInfo struct {
-	Symbol                              string                              `json:"symbol"`
-	ImpliedVolatilityIndex              float64                             `json:"implied-volatility-index"`
-	ImpliedVolatilityIndex5DayChange    float64                             `json:"implied-volatility-index-5-day-change"`
-	ImpliedVolatilityRank               float64                             `json:"implied-volatility-rank"`
-	ImpliedVolatilityPercentile         float64                             `json:"implied-volatility-percentile"`
-	Liquidity                           float64                             `json:"liquidity"`
-	LiquidityRank                       float64                             `json:"liquidity-rank"`
-	LiquidityRating                     int32                               `json:"liquidity-rating"`
-	OptionExpirationImpliedVolatilities []OptionExpirationImpliedVolatility `json:"option-expiration-implied-volatilities"`
+	Symbol                string `json:"symbol"`
+	ImpliedVolatilityRank string `json:"implied-volatility-index-rank"`
 }
 
 type MarketMetricInfoItems struct {
 	Items []MarketMetricInfo `json:"items"`
 }
 
-type MarketMetricInfoData struct {
-	Data MarketMetricInfoItems `json:"data"`
-}
-
 type MarketMetricInfoResponse struct {
-	Data MarketMetricInfoData `json:"data"`
+	Data MarketMetricInfoItems `json:"data"`
 }
 
 var marketMetricsUrl = fmt.Sprintf("%s/market-metrics", baseUrl)
@@ -47,13 +29,13 @@ func (t *TTClient) GetMarketMetrics(symbols []string) (*MarketMetricInfoResponse
 		return nil, err
 	}
 
-	responseBody := MarketMetricInfoResponse{}
+	responseBody := &MarketMetricInfoResponse{}
 
-	err = convertResponseToJson(resp, &responseBody)
+	err = convertResponseToJson(resp, responseBody)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &responseBody, nil
+	return responseBody, nil
 }
