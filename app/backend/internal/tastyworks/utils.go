2265dd391a3675c8240ad89ebefe2319f5beaade
Jelani Jackson <jelani.jackson@zapier.com>
2024-07-27 14:22:39 -0400

    So much cleaner

--- app/backend/internal/tastyworks/utils.go
+++ app/backend/internal/tastyworks/utils.go
@@ -3,28 +3,25 @@ package tastyworks
 import (
 	"bytes"
 	"encoding/json"
-	"io"
 	"net/http"
 )
 
 func convertResponseToJson[T any](resp http.Response, jsonBody *T) error {
-	bodyBytes, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
 
-	if err != nil {
-		return err
-	}
+	err := json.NewDecoder(resp.Body).Decode(jsonBody)
 
-	return json.Unmarshal(bodyBytes, jsonBody)
+	return err
 }
 
 func prepareRequestBody[T any](requestData T) (bytes.Buffer, error) {
-	jsonBytes, err := json.Marshal(requestData)
+	body := new(bytes.Buffer)
+
+	err := json.NewEncoder(body).Encode(requestData)
 
 	if err != nil {
 		return bytes.Buffer{}, err
 	}
-
-	body := bytes.NewBuffer(jsonBytes)
 
 	return *body, nil
 }
